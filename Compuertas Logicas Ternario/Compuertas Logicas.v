7423ce1153237b19ffc5af3f1894a6d7b2a2f413
Aldo Alexandro Vargas Meza <aldo.vargas.meza94@gmail.com>
2021-03-10 23:46:45 -0600

    Update Compuertas Logicas.v
    Easier to maintain

--- Compuertas Logicas Ternario/Compuertas Logicas.v
+++ Compuertas Logicas Ternario/Compuertas Logicas.v
@@ -1,28 +1,31 @@
 /*Modulo Compuertas Logicas
   Selector
   Funciones Logicas: AND, OR, XOR, NAND, NOR, XNOR
-  Señal Activacion
+  SeÃ±al Activacion
   3 Entradas
   Asincrona
 */
 
-module compuertasLogicas(ent1, ent2, ent3, sel, sal, act);
-	input wire 	ent1, ent2, ent3;
+module compuertasLogicas(in1, in2, in3, sel, out, act);
+	input wire 	in1, in2, in3;
 	input wire 	act;
 	input wire [2:0]sel;
-	output reg 	sal;
+	output reg 	out;
 
 	always@(*)begin
-		
-		sal = 	(sel == 3'b001)? ( ent1 & ent2 & ent3 ):		//AND
-			(sel == 3'b010)? ( ent1 | ent2 | ent3 ):		//OR
-			(sel == 3'b011)? ( ent1 ^ ent2 ^ ent3 ):		//XOR
-			
-			(sel == 3'b100)? !( ent1 & ent2 & ent3 ):		//NAND
-			(sel == 3'b101)? !( ent1 | ent2 | ent3 ):		//NOR
-			(sel == 3'b110)? !( ent1 ^ ent2 ^ ent3 ):		//XNOR
-							  (1'b0);		//Default
-
+		if(sel == 3'b001) // AND
+			out = ( ent1 & ent2 & ent3 );
+		else if(sel == 3'b010) // OR
+			out = ( ent1 | ent2 | ent3 );
+		else if(sel == 3'b011) // XOR
+			out = ( ent1 ^ ent2 ^ ent3 );
+		else if(sel == 3'b100) // NAND
+			out = !( ent1 & ent2 & ent3 );
+		else if(sel == 3'b101) // NOR
+			out = !( ent1 | ent2 | ent3 );
+		else if(sel == 3'b110) // XNOR
+			out = !( ent1 ^ ent2 ^ ent3 );
+		end
 	end
 
 endmodule
